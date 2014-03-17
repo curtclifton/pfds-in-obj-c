a09f7a57e4ded83c9f3926c7f4c3deabb881c426
Curt Clifton <curt.clifton@mac.com>
2014-03-16 17:52:58 -0700

    Added Xcode workspace and a framework project within it.
    Template code only so far, but it builds and the one test (intentionally) fails.

---
+++ PurelyFunctionDS/PurelyFunctionDS/PurelyFunctionDS.h
@@ -0,0 +1,13 @@
+//
+//  PurelyFunctionDS.h
+//  PurelyFunctionDS
+//
+//  Created by Curt Clifton on 3/16/14.
+//  Copyright (c) 2014 curtclifton.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PurelyFunctionDS : NSObject
+
+@end
---
+++ PurelyFunctionDS/PurelyFunctionDS/PurelyFunctionDS.m
@@ -0,0 +1,13 @@
+//
+//  PurelyFunctionDS.m
+//  PurelyFunctionDS
+//
+//  Created by Curt Clifton on 3/16/14.
+//  Copyright (c) 2014 curtclifton.net. All rights reserved.
+//
+
+#import "PurelyFunctionDS.h"
+
+@implementation PurelyFunctionDS
+
+@end
