eb75a023f90d32d4cebb1cb2d56bea6198347259
Curt Clifton <curt.clifton@mac.com>
2014-06-17 19:42:28 -0700

    Adding template project for Swift work.

---
+++ PurelySwiftDS/PurelySwiftDS/PurelySwiftDS.h
@@ -0,0 +1,19 @@
+//
+//  PurelySwiftDS.h
+//  PurelySwiftDS
+//
+//  Created by Curt Clifton on 6/17/14.
+//  Copyright (c) 2014 curtclifton.net. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PurelySwiftDS.
+FOUNDATION_EXPORT double PurelySwiftDSVersionNumber;
+
+//! Project version string for PurelySwiftDS.
+FOUNDATION_EXPORT const unsigned char PurelySwiftDSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PurelySwiftDS/PublicHeader.h>
+
+
